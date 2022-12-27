8e0161e2a1530b52819c609963cb462a49fccb9a
Nikola Jovanovic <jovkemax@gmail.com>
2022-12-27 19:58:49 +0100

    Added comments

--- WebScrape/Controllers/HomeController.cs
+++ WebScrape/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebScrape.Controllers
             _logger = logger;
         }
 
-
+        //Returns absolute URL from a base website URL plus relativeUrl to a file or directory and returns as a string
         private static String relativeToAbsolute(String baseUrl, String relativeUrl)
         {
             Uri baseUri = new Uri(baseUrl);
@@ -25,10 +25,13 @@ namespace WebScrape.Controllers
             return uri.ToString();
         }
 
+        //Makes a Http request for a website with some specific headers we need(otherwise won't work for bakerhughes site)
         private static HttpWebRequest makeRequest(string url, int timeout)
         {
+            //Make request
             HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(url);
 
+            //Set header attributes
             req.KeepAlive = true;
             req.Accept = "application/json, text/plain, */*";
             req.Timeout = req.ReadWriteTimeout = timeout;
@@ -40,6 +43,7 @@ namespace WebScrape.Controllers
             return req;
         }
 
+        //Returns a response stream from a request
         private static async Task<Stream> getStreamFromRequest(string trueUrl)
         {
             var request = makeRequest(trueUrl, 50000);
@@ -48,6 +52,7 @@ namespace WebScrape.Controllers
             return stream;
         } 
 
+        //Gets stream of file and saves it to given filePath
         private static async Task downloadFileAndSaveToLocalDiskAsync(string trueUrl,string filePath)
         {
             Stream stream = await getStreamFromRequest(trueUrl);
@@ -58,18 +63,23 @@ namespace WebScrape.Controllers
             
         }
 
+        //Gets stream of html, loads it to a HtmlAgilityPack.HtmlDocument and then searches for the file we need by title
         private static async Task<string> getUrlOfFileAsync(string url)
         {
             Stream stream = await getStreamFromRequest(url);
             StreamReader streamReader = new StreamReader(stream);
             string html = streamReader.ReadToEnd();
+            //Make doc and load html
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(html);
+            //Search by title
             var link = doc.DocumentNode.SelectSingleNode("//a[@title='Worldwide Rig Count Nov 2022.xlsx']");
             string fileUrl = link.Attributes["href"].Value;
+            //Return relative url
             return fileUrl;
         }
 
+        //Opens .xlsx file, modifies to last 2 years and saves as CSV in same directory
         private static void modifyToLast2YearsAndSaveAsCSV(string filePath)
         {
             using (ExcelEngine engine = new())
@@ -82,6 +92,7 @@ namespace WebScrape.Controllers
 
                 IWorkbook workbook = excelApp.Workbooks.Open(fileStream);
 
+                //Delete rows we don't need
                 var worksheet = workbook.Worksheets[0];
                 worksheet.DeleteRow(1, 6);
                 worksheet.DeleteRow(29, 690);
@@ -90,6 +101,8 @@ namespace WebScrape.Controllers
                 fileStream.Close();
 
                 string value = @"C:\temp\new.csv";
+
+                //Save file as CSV (Comma Separated Value)
                 using (FileStream fs = new(value, FileMode.Create))
                 {
                     worksheet.SaveAs(fs, ",");
@@ -99,21 +112,26 @@ namespace WebScrape.Controllers
             }
         }
 
+        //This will run first because it's specified in Program.cs
         public async Task<IActionResult> Index()
         {
+            //URL of the website we need to scrape for a file and path to save that file
             string url = "https://bakerhughesrigcount.gcs-web.com/intl-rig-count";
             string filePath = @"C:\temp\myfile.xlsx";
          
             //Free licence of Syncfusion package
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt/QHRqVVhjVFpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF9iS3xSdEVnW39ed3ZSRg==;Mgo+DSMBMAY9C3t2VVhkQlFadVdJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0dhWH1edHZVRmdbUUQ=\r\n");
             
-            
+            //Get relative URL of the file we need
             string fileUrl = await getUrlOfFileAsync(url);
 
+            //Get the whole absolute URL of the file
             string trueUrl = relativeToAbsolute(url, fileUrl);
 
+            //Download the file and save it to a local disk
             await downloadFileAndSaveToLocalDiskAsync(trueUrl, filePath);
            
+            //Take last 2 years of the file and save it to a new CSV type file
             modifyToLast2YearsAndSaveAsCSV(filePath);
 
             return View();
